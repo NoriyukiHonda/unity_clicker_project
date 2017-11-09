ba4e4aa3e599aa0143a883242c8263a8e46cfcde
noriyuki.honda <noriyuki.honda@playnext-lab.co.jp>
2017-11-09 09:35:51 +0900

    add 2DUnityProject

---
+++ Assets/Scripts/ClickerManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickerManager : MonoBehaviour
+{
+	[SerializeField] Text fpsText;
+	[SerializeField] Text clickAmountText;
+	[SerializeField] Text objectAmountText;
+
+	int counter = 0;
+	int clickAmount = 0;
+	int objectAmount = 0;
+
+	void Update()
+	{
+		if(Input.GetMouseButton(0))
+		{
+			clickAmount++;
+			var localVector3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+			// プレハブを取得
+			var prefab = (GameObject)Resources.Load("Prefabs/ClickerObject");
+			for(int rad = 0; rad < 360; rad+=30)
+			{
+				// prefabを八方向にInstantiate
+				var item = Instantiate(prefab, localVector3, Quaternion.identity, this.gameObject.transform);
+				var clickerObject = item.GetComponent<ClickerObject>();
+				clickerObject.Initialize(rad, 100);
+				objectAmount++;
+			}
+		}
+
+		counter++;
+		if(counter >= 30)
+		{
+			fpsText.text = "FPS:" + (1f / Time.deltaTime).ToString("f2");
+			counter = 0;
+		}
+
+		clickAmountText.text = "クリック回数:" + clickAmount.ToString();
+
+		objectAmountText.text = "オブジェクト数:" + objectAmount.ToString();
+	}
+}
---
+++ Assets/Scripts/ClickerObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickerObject : MonoBehaviour
+{
+	public void Initialize(float direction, float speed)
+	{
+		var rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+		Vector2 v = new Vector2();
+		v.x = Mathf.Cos(Mathf.Deg2Rad * direction) * speed;
+		v.y = Mathf.Sin(Mathf.Deg2Rad * direction) * speed;
+		rigidBody.velocity = v;
+	}
+
+	void Update()
+	{
+		if((0 > this.gameObject.transform.position.x || this.gameObject.transform.position.x > Screen.width) ||
+		   (0 > this.gameObject.transform.position.y || this.gameObject.transform.position.y > Screen.height))
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
