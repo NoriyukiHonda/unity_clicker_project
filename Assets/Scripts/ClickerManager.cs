3ae2f9db6038ba9a1363743da62270a1d734a723
noriyuki.honda <noriyuki.honda@playnext-lab.co.jp>
2018-04-18 21:17:20 +0900

    UGUIでアンカーを設定し、どの画面サイズでも文字が見切れないようにした TotalDamageとPowerとTotalClickを追加 PlayerPrefsでデータを保存するようにした

--- Assets/Scripts/ClickerManager.cs
+++ Assets/Scripts/ClickerManager.cs
@@ -5,41 +5,101 @@ using UnityEngine.UI;
 
 public class ClickerManager : MonoBehaviour
 {
+	const string TOTAL_CLICK_KEY = "TotalClick";
+	const string POWER_KEY = "Power";
+	const string TOTAL_DAMAGE_KEY = "TotalDamage";
 	[SerializeField] Text fpsText;
-	[SerializeField] Text clickAmountText;
-	[SerializeField] Text objectAmountText;
+	[SerializeField] Text totalClickText;
+	[SerializeField] Text powerText;
+	[SerializeField] Text totalDamageText;
 
 	int counter = 0;
-	int clickAmount = 0;
-	int objectAmount = 0;
+	int totalClick = 0;
+	int power = 0;
+	int totalDamage = 0;
+
+	void Start()
+	{
+		LoadData();
+	}
+
+	void LoadData()
+	{
+		totalClick = PlayerPrefs.GetInt(TOTAL_CLICK_KEY, 0);
+		power = PlayerPrefs.GetInt(POWER_KEY, 0);
+		totalDamage = PlayerPrefs.GetInt(TOTAL_DAMAGE_KEY, 0);
+	}
 
 	void Update()
 	{
-		if(Input.GetMouseButton(0))
+		if(Input.GetMouseButtonDown(0))
 		{
-			clickAmount++;
-			var localVector3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
-			// プレハブを取得
-			var prefab = (GameObject)Resources.Load("Prefabs/ClickerObject");
-			for(int rad = 0; rad < 360; rad+=30)
-			{
-				// prefabを八方向にInstantiate
-				var item = Instantiate(prefab, localVector3, Quaternion.identity, this.gameObject.transform);
-				var clickerObject = item.GetComponent<ClickerObject>();
-				clickerObject.Initialize(rad, 100);
-				objectAmount++;
-			}
+			AddTotalClick();
+			CauseDamage();
+			InstantiateItems();
 		}
 
+		DrawText();
+	}
+
+	void InstantiateItems()
+	{
+		var localVector3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+		var prefab = (GameObject)Resources.Load("Prefabs/ClickerObject");
+		for (int rad = 0; rad < 360; rad += 30)
+		{
+			// 八方向にInstantiate
+			var item = Instantiate(prefab, localVector3, Quaternion.identity, this.gameObject.transform);
+			var clickerObject = item.GetComponent<ClickerObject>();
+			clickerObject.Initialize(rad, 100);
+		}
+	}
+
+	void DrawText()
+	{
 		counter++;
-		if(counter >= 30)
+		if (counter >= 30)
 		{
 			fpsText.text = "FPS:" + (1f / Time.deltaTime).ToString("f2");
 			counter = 0;
 		}
 
-		clickAmountText.text = "クリック回数:" + clickAmount.ToString();
+		totalClickText.text = "クリック総数:" + totalClick.ToString();
 
-		objectAmountText.text = "オブジェクト数:" + objectAmount.ToString();
+		powerText.text = "パワー:" + power.ToString();
+		totalDamageText.text = "トータルダメージ:" + totalDamage.ToString();
+	}
+
+	void AddTotalClick()
+	{
+		totalClick++;
+	}
+
+	void CauseDamage()
+	{
+		totalDamage += power;
+	}
+
+	void OnDestroy()
+	{
+		SaveData();
+	}
+
+	void SaveData()
+	{
+		PlayerPrefs.SetInt(TOTAL_CLICK_KEY, totalClick);
+		PlayerPrefs.SetInt(POWER_KEY, power);
+		PlayerPrefs.SetInt(TOTAL_DAMAGE_KEY, totalDamage);
+		PlayerPrefs.Save();
+	}
+
+	public void OnClick()
+	{
+		AddPower();
+	}
+
+	void AddPower()
+	{
+		power++;
 	}
 }
